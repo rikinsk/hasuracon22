8057c8487197daf49439b8f31226ec1867889b7d
rikinsk <rikin@hasura.io>
2022-06-23 22:54:44 +0530

    add comment to poll oprions

---
+++ hasura/migrations/default/1655895299232_insert_into_public_option/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."option"("id", "poll_id", "text", "comment") VALUES (E'5a7a3663-40a7-4723-9036-948b760b4d9b', E'98277113-a7a2-428c-9c8b-0fe7a91bf42c', E'Rails', E'Ruby framework');
